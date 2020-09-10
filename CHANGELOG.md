1cd3798738e6222c9e9a7a837b3feeeec78fa91e
Neil Stoker <neil.stoker@gmail.com>
2020-09-10 23:26:50 +0100

    Hook up a database

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## Chapter 6
 
 - Added sequel and sqlite3 gems.
+- Added migration files. Requires the `:db` tag for tests that need to touch the database.
 
 ## Chapter 5
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,10 @@ RSpec.configure do |config|
   # From the tutorial, filter out framework warnings we don't need at the moment
   config.filter_gems_from_backtrace 'rack', 'rack-test', 'sequel', 'sinatra'
   
+  config.when_first_matching_example_defined(:db) do
+    require_relative 'support/db'
+  end
+  
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
